257c9850d5048e1bd1e6d91b43f1c82a122e973f
Nolen Johnson <johnsonnolen@gmail.com>
2021-02-24 20:56:15 -0500

    zenfone7: Drop PRODUCT_PACKAGES_DEBUG entry
    * unnecessary
    
    Change-Id: I22ae2c744ec55168b8367c826e55110e7534466e

--- device.mk
+++ device.mk
@@ -117,9 +117,6 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
     android.hardware.boot-service.qti.recovery
 
-PRODUCT_PACKAGES_DEBUG += \
-    bootctl
-
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
@@ -351,9 +348,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_HOST_PACKAGES += \
     brillo_update_payload
-
-PRODUCT_PACKAGES_DEBUG += \
-    update_engine_client
 
 # Vibrator
 PRODUCT_PACKAGES += \
