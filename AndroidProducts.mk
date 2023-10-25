eb28465eae23e1c3e8b6ed0f32f96e63679dbca6
Greg Kaiser <gkaiser@google.com>
2023-10-25 16:31:29 -0600

    zenfone7: Fix lunch choices for git_main
    We add "trunk_staging-" for all COMMON_LUNCH_CHOICES so they are
    valid in the trunk stable setup.
    
    Bug: 307738446
    Test: lunch
    Change-Id: I400392ba9cf7e6d35af78d6b668d52abee27497d

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -3,7 +3,7 @@ PRODUCT_MAKEFILES := \
 
 COMMON_LUNCH_CHOICES := \
     omni_zenfone7-user \
-    omni_zenfone7-userdebug \
+    omni_zenfone7-trunk_staging-userdebug \
     omni_zenfone7-eng
     
     
