48beb727e4457569072da1937e3b5ee1e18566ed
micky387 <mickaelsaibi@free.fr>
2024-03-18 21:34:31 -0400

    zenfone7: Fix COMMON_LUNCH_CHOICES release
    trunk_staging is not shipped by AOSP yet . Only ap1a is available but it's really bad to hardcoded this release version.
    Not thx Google.... :(
    
    Change-Id: Ib89dbffaa9a2ae0db6f3ac58920b12e5fca71520

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,8 +2,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_zenfone7.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_zenfone7-user \
-    omni_zenfone7-trunk_staging-userdebug \
-    omni_zenfone7-eng
-    
-    
+    omni_zenfone7-ap1a-user \
+    omni_zenfone7-ap1a-nk_staging-userdebug \
+    omni_zenfone7-ap1a-eng
