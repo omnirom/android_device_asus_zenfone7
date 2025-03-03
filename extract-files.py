2e0eac441f686e0f67cfff233e0a815cc5b58093
micky387 <mickaelsaibi@free.fr>
2025-03-02 19:43:20 -0500

    zenfone7: Drop sm8250 qcom-caf soong namespace in extract-files
    Change-Id: I1eda4d2c6217641953e9af8795afda96f13cdec4
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- extract-files.py
+++ extract-files.py
@@ -19,7 +19,6 @@ from extract_utils.main import (
 )
 
 namespace_imports = [
-    'hardware/qcom-caf/sm8250',
     'hardware/qcom-caf/wlan',
     'vendor/qcom/opensource/display',
     'vendor/qcom/opensource/commonsys/display',
