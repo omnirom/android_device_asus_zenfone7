9930017cd8d395d6a6ac299a7c3bdf401b3866f8
micky387 <mickaelsaibi@free.fr>
2025-10-01 16:41:36 -0400

    zenfone7: Build ipacm opensource fron source
    Change-Id: I46f2aa24444be63be9ca3c5e43cbba9a5d37d1ad
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- device.mk
+++ device.mk
@@ -215,6 +215,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     keylayout_data_zenfone7.kl
 
+# IPACM
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml
+
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := true
 
